c567c58929c2f2fde4e0b9b13eab0ec4f7442abf
Luca Borzacchiello <lucaborza@gmail.com>
2020-01-04 12:59:52 +0100

    first commit

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from binaryninja import BinaryView
+import sys
+import os
+
+class WrapperView(BinaryView):
+    name = "Dummy Wrapper"
+    """ Dummy view to wrap the initialization of a BinaryView """
+    def __init__(self, data):
+        BinaryView.__init__(self, parent_view = data, file_metadata = data.file)
+        self.raw = data
+
+    @classmethod
+    def is_valid_for_data(self, data):
+        path = os.path.dirname(data.file.filename) + "/bninja_scripts"
+        print("LocalScriptLoader: Adding {} to path".format(
+                path
+            )
+        )
+        sys.path.append(path)
+        # Just a wrapper. Always fail
+        return False
+
+WrapperView.register()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Local Script Loader
+Just an hack to add `/path/to/binary/bninja_scripts` to python path. 
+The python files in `bninja_scripts` are accessible in the embedded python console.
